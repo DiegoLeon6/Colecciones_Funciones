6f078871a9918b460739b6feb615e1a5a828ff9c
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:20:53 -0500

    Fix indentation in invertir function

--- funcio7.py
+++ funcio7.py
@@ -1,9 +1,10 @@
 def invertir(cadena):
-    if len(cadena) == 0:  # Caso base: cadena vacía
+    if len(cadena) == 0: 
         return ""
     else:
-        return invertir(cadena[1:]) + cadena[0]  # Llamada recursiva
+        return invertir(cadena[1:]) + cadena[0]
 
 texto = input("Ingrese una cadena: ")
 print("Cadena invertida:", invertir(texto))
+
 
