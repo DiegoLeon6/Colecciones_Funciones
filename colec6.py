38e4f8cfcacdc5d4ea0daf3800b82768b9aa43cd
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:14:31 -0500

    Remove unnecessary line breaks in colec6.py

--- colec6.py
+++ colec6.py
@@ -1,8 +1,8 @@
 entrada = input("Ingresa los elementos separados por comas: ")
 
-# Convertir la cadena a lista
 lista = [x.strip() for x in entrada.split(",")]
 
 tupla_resultado = tuple(set(lista))
 
 print(tupla_resultado)
+
