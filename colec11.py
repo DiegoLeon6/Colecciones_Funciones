ddd57d3ab66cbf4d30e9871c74d050d3c0db00ac
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:05:24 -0500

    Remove unnecessary comments and whitespace

--- colec11.py
+++ colec11.py
@@ -1,15 +1,15 @@
-# Ingresar conjunto A
+
 entradaA = input("Ingrese los elementos del conjunto A separados por espacio: ").split()
 A = set(int(x) for x in entradaA)
 
-# Ingresar conjunto B
+
 entradaB = input("Ingrese los elementos del conjunto B separados por espacio: ").split()
 B = set(int(x) for x in entradaB)
 
-# Operaciones con conjuntos
+
 union = A | B          # Unión
 diferencia = A - B     # Diferencia
 
-# Resultados
 print("Unión:", union)
 print("Diferencia:", diferencia)
+
