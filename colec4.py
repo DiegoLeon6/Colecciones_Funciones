632af02377835fdb600831f0af60ec436d315f14
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:13:39 -0500

    Reformat code for matrix input and addition

--- colec4.py
+++ colec4.py
@@ -1,8 +1,8 @@
-# Pedir tamaño de la matriz
+
 filas = int(input("Número de filas: "))
 columnas = int(input("Número de columnas: "))
 
-# Ingresar matriz A
+
 print("Ingrese los valores de la matriz A:")
 A = []
 for i in range(filas):
@@ -12,7 +12,7 @@ for i in range(filas):
         fila_num.append(int(n))
     A.append(fila_num)
 
-# Ingresar matriz B
+
 print("Ingrese los valores de la matriz B:")
 B = []
 for i in range(filas):
@@ -22,7 +22,6 @@ for i in range(filas):
         fila_num.append(int(n))
     B.append(fila_num)
 
-# Sumar matrices A + B
 S = []
 for i in range(filas):
     fila_suma = []
@@ -30,7 +29,8 @@ for i in range(filas):
         fila_suma.append(A[i][j] + B[i][j])
     S.append(fila_suma)
 
-# Mostrar resultado
+
 print("Suma de las matrices:")
 print(S)
+
 
