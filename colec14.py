7cb73ec746cff3bbaaf0add2b37b9488d88e2bb6
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:28:14 -0500

    Update colec14.py

--- colec14.py
+++ colec14.py
@@ -6,8 +6,8 @@ A = set(int(x) for x in entradaA)
 entradaB = input("Ingrese los elementos del conjunto B separados por espacio: ").split()
 B = set(int(x) for x in entradaB)
 
-
-dif_sim = A ^ B  # También se puede usar: A.symmetric_difference(B)
+dif_sim = A ^ B 
 
 print(dif_sim)
+
 
