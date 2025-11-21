08cf404882e9d304c80e25812404956afece2e66
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:12:08 -0500

    Update colec19.py

--- colec19.py
+++ colec19.py
@@ -1,12 +1,12 @@
-# Pedir al usuario los pares clave:valor
+
 p1 = input("Par 1 (clave: valor): ")
 p2 = input("Par 2 (clave: valor): ")
 p3 = input("Par 3 (clave: valor): ")
 
-# Diccionario original
+
 original = {}
 
-# Procesar cada entrada
+
 c1, v1 = p1.split(":")
 original[c1.strip()] = v1.strip()
 
@@ -16,10 +16,11 @@ original[c2.strip()] = v2.strip()
 c3, v3 = p3.split(":")
 original[c3.strip()] = v3.strip()
 
-# Invertir diccionario
+
 invertido = {}
 
 for clave, valor in original.items():
     invertido[valor] = clave
 
 print(invertido)
+
