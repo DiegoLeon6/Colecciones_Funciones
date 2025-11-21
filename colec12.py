61732de13a594649fb208c6ace7ce7bd8c0e1334
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:05:38 -0500

    Remove unnecessary comments in colec12.py

--- colec12.py
+++ colec12.py
@@ -1,8 +1,7 @@
-# Ingresar la frase
+
 frase = input("Ingrese una frase: ")
 
-# Separar en palabras y convertir a conjunto
 palabras_unicas = set(frase.split())
 
-# Mostrar resultado
 print(palabras_unicas)
+
