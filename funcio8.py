c203eaf85cf7414477246bc582349b38b4c9d591
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:21:14 -0500

    Update funcio8.py

--- funcio8.py
+++ funcio8.py
@@ -1,18 +1,17 @@
-# Función recursiva para invertir cadena
+
 def invertir(cadena):
     if len(cadena) == 0:
         return ""
     else:
         return invertir(cadena[1:]) + cadena[0]
 
-# Función para verificar palíndromo
 def es_palindromo(cadena):
-    cadena = cadena.lower().replace(" ", "")  # Ignora mayúsculas y espacios
+    cadena = cadena.lower().replace(" ", "") 
     return cadena == invertir(cadena)
 
-# Ejemplo de uso
 texto = input("Ingrese una cadena: ")
 if es_palindromo(texto):
     print("Es palíndromo")
 else:
     print("No es palíndromo")
+
