c62cea94bf748f5a6e68443108e44a4d92396f87
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:17:40 -0500

    Fix indentation and formatting in factorial function

--- funcio1.py
+++ funcio1.py
@@ -3,7 +3,7 @@ def factorial(n):
     for i in range(1, n + 1):
         resultado = resultado * i
     return resultado
-
-# Ejemplo de uso:
+    
 numero = int(input("Ingrese un número: "))
 print(factorial(numero))
+
