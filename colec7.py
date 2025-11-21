08694fce8af056a5a428e944ff354e662a05f750
DiegoLeon6 <leonurbanodiego@gmail.com>
2025-11-21 18:14:56 -0500

    Refactor input and range checking code

--- colec7.py
+++ colec7.py
@@ -1,17 +1,15 @@
-# Pedir la coordenada
+
 x = int(input("Ingrese x: "))
 y = int(input("Ingrese y: "))
 
-# Crear la tupla
 coordenada = (x, y)
 
-# Pedir el rango
 limite_inferior = int(input("Límite inferior del rango: "))
 limite_superior = int(input("Límite superior del rango: "))
 
-# Verificar si está dentro del rango
 if limite_inferior <= coordenada[0] <= limite_superior and \
    limite_inferior <= coordenada[1] <= limite_superior:
     print("Dentro del rango")
 else:
     print("Fuera del rango")
+
